5157db4cfc1d023549e03d31814c5d4342105186
Ikenna Richard <oguejioforrichard@gmail.com>
2023-06-07 23:16:03 -1100

    Update schema file

--- schema.sql
+++ schema.sql
@@ -27,25 +27,19 @@ CREATE TABLE species(
     name TEXT
 );
 
--- remove species column
---@block
 ALTER TABLE animals DROP COLUMN species;
 
 
--- add species_id column
---@block
 ALTER TABLE animals ADD COLUMN species_id INT;
 
---@block
 ALTER TABLE animals 
 ADD CONSTRAINT species_fk
 FOREIGN KEY(species_id) 
 REFERENCES species(id);
 
---@block
+
 ALTER TABLE animals ADD COLUMN owner_id INT;
 
---@block
 ALTER TABLE animals 
 ADD CONSTRAINT owners_fk
 FOREIGN KEY(owner_id) 
